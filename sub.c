2426fae7b8856b0103ee94eecedc62037630202a
21683936 <yoshinori.hayakawa.vy@hitachi-solutions.com>
2024-08-02 15:12:18 +0900

    Update sub.c

--- sub.c
+++ sub.c
@@ -1,4 +1,25 @@
-void sub(void){
 
+
+void sub(void){
+  
+	int i;
+	for (i=1; i<=END_VALUE; i++){
+		SUM=SUM+i;
+		// PVSCL:IFCOND(IntermediateResults)
+		printf("%d,",SUM);
+		// PVSCL:ENDCOND
+	}
+
+	// PVSCL:IFCOND(IntermediateResults)
+	// PVSCL:IFCOND(IntermediateResults->Format='number')
+	printf("%d\n",SUM);
+	// PVSCL:ELSEIFCOND(IntermediateResults->Format='formula')
+	printf("<ans>: %d\n",SUM);
+	// PVSCL:ELSEIFCOND(IntermediateResults->Format='sentence')
+	printf("合計は、%d です。\n",SUM);
+	// PVSCL:ELSECOND
+	printf("none\n");
+	// PVSCL:ENDCOND
+	// PVSCL:ENDCOND
 
 }
